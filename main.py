9e4bb1dc7f024ba68635a8cf51f4ce776fe340c7
Matthew Gillespie <gillespiem@braindeadprojects.com>
2023-04-19 16:44:56 -0400

    Adding a watchdog thread to main

--- main.py
+++ main.py
@@ -4,11 +4,23 @@ from wifi import Wifi
 from machine import Pin
 import time
 import led_smokesignal
+import _thread
 
 
 
-if __name__ == "__main__":
+def watchdog_thread():
+    wdt = machine.WDT(timeout=8000)
+    while True:
+        print("Petting the dog")
+        wdt.feed()
+        time.sleep(5)
+
+def main_thread():
     wifi = Wifi()
     api = Api_handler_gardenbrain()
-    webserver = BotwebServer(api)
+    webserver = BotwebServer(api)    
 
+if __name__ == "__main__":
+    second_thread = _thread.start_new_thread(watchdog_thread, ())
+    main_thread()
+    #thread_two()
