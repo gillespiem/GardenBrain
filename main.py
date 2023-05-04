921c81d2b7cb224ab1f4c8b92704a0722ac691fd
Matthew Gillespie <gillespiem@braindeadprojects.com>
2023-05-04 19:34:02 -0400

    Adding MQTT thread

--- main.py
+++ main.py
@@ -5,8 +5,9 @@ from machine import Pin
 import time
 import _thread
 from gardenlog import GardenLog
-
-
+from mqtt_gardenbrain import Mqtt
+import gc
+import os
 
 
 def watchdog_thread():
@@ -25,23 +26,37 @@ def watchdog_thread():
         wdt.feed()
         time.sleep(5)
 
+def mqtt_thread():
+    mqtt = Mqtt(glog, api)
+
+
 def main_thread():
     try:
         #wifi = Wifi()
-        api = Api_handler_gardenbrain(glog)
+        #api = Api_handler_gardenbrain(glog) #Testing to see if I can share the api across threads like I am with glog.
         webserver = BotwebServer(glog, api)
     except Exception as e:
         print("Exception thrown resetting")
         print(e.message)
         machine.reset()
 
+def bootstats(glog):
+    s = os.statvfs('/')
+    glog.message(f"Memory: {gc.mem_alloc()} of {gc.mem_free()} bytes used.")
+    glog.message(f"Free storage: {s[0]*s[3]/1024} KB")
+    glog.message(f"CPU Freq: {machine.freq()/1000000}Mhz")
 
 if __name__ == "__main__":
     
     glog = GardenLog()
     glog.message("Now initializing")
     
+    api = Api_handler_gardenbrain(glog)
+
+    bootstats(glog)
+    
     wifi = Wifi(glog)
-    #second_thread = _thread.start_new_thread(watchdog_thread, ())
+    second_thread = _thread.start_new_thread(mqtt_thread, ())
  
     main_thread()
+    
